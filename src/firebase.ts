ddabf027cabb6f93c09a23150d7ceb07fc1a5c61
aken <aken@kakao.com>
2025-10-12 18:18:26 +0900

    1.3 Firebase Project Setup

---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9us56OLEvQWymeuIvO1_ha-Pm43Umv5g",
+  authDomain: "nwitter-reloaded-2e665.firebaseapp.com",
+  projectId: "nwitter-reloaded-2e665",
+  storageBucket: "nwitter-reloaded-2e665.firebasestorage.app",
+  messagingSenderId: "473821755697",
+  appId: "1:473821755697:web:f4c82d64bceb48bf725810",
+  measurementId: "G-ZELWN5NZCE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
