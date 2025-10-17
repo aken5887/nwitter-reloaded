ec0bd900c1c8c3ca6fce086cd46eb9ae1baae2a4
aken <aken@kakao.com>
2025-10-17 20:29:00 +0900

    2.2 Create Password

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,38 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./routes/home";
 import Profile from "./routes/profile";
 import Login from "./routes/login";
 import CreateAccount from "./routes/create-account";
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import { useEffect, useState } from "react";
 import LoadingScreen from "./components/loading-screen";
+import { auth } from "./firebase";
 
 const router = createBrowserRouter([
   {
-    path : "/",
-    element : <Layout/>,
-    children : [
+    path: "/",
+    element: <Layout />,
+    children: [
       {
-        path : "/",
-        element : <Home/>
-      }, {
-        path : "profile",
-        element : <Profile/>
-      }
-    ]
-  }, 
-  {
-    path : "login",
-    element : <Login/>
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
   },
   {
-    path : "create-account",
-    element : <CreateAccount/>
-  }  
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "create-account",
+    element: <CreateAccount />,
+  },
 ]);
 
 const GlobalStyles = createGlobalStyle`
@@ -45,24 +47,31 @@ const GlobalStyles = createGlobalStyle`
   }
   `;
 
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
 function App() {
   const [isLoading, setLoading] = useState(true);
-  
+
   const init = async () => {
     // wait for firebase
-    setTimeout(() => setLoading(false), 2000);
-  }
+    await auth.authStateReady();
+    setLoading(false);
+  };
 
   useEffect(() => {
     init();
   }, []);
 
   return (
-    <>
-      <GlobalStyles/>
-      {isLoading ? <LoadingScreen/> : <RouterProvider router={router}/>}
-    </>
-  )
+    <Wrapper>
+      <GlobalStyles />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+    </Wrapper>
+  );
 }
 
-export default App
+export default App;
--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,3 +1,129 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { auth } from "../firebase";
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 420px;
+  padding: 50px 0px;
+`;
+const Title = styled.h1`
+  font-size: 42px;
+`;
+const Form = styled.form`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  padding: 10px 20px;
+  border-radius: 50px;
+  width: 100%;
+  font-size: 16px;
+  &[type="submit"] {
+    cursor: pointer;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
+
+const Error = styled.span`
+  font-weight: 600;
+  color: tomato;
+`;
+
 export default function CreateAccount() {
-    return <h1>Create account</h1>
+  const navigate = useNavigate();
+  const [isLoading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    console.log("####", name, value);
+    if (name === "name") {
+      setName(value);
+    } else if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (isLoading || name === "" || email === "" || password === "") return;
+    try {
+      // create account
+      setLoading(true);
+      const credentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      console.log(credentials.user);
+
+      await updateProfile(credentials.user, {
+        displayName: name,
+      });
+
+      navigate("/");
+    } catch (e) {
+      console.log(e);
+      setError(JSON.stringify(e));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Title>Join 𝕏</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          onChange={onChange}
+          name="name"
+          type="text"
+          placeholder="Name"
+          value={name}
+          required
+        />
+        <Input
+          onChange={onChange}
+          name="email"
+          type="email"
+          placeholder="Email"
+          value={email}
+          required
+        />
+        <Input
+          onChange={onChange}
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+          required
+        />
+        <Input
+          type="submit"
+          value={isLoading ? "Loading..." : "Create Account"}
+        />
+      </Form>
+      {error !== "" ? <Error>{error}</Error> : null}
+    </Wrapper>
+  );
 }
